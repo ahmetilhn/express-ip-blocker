f34a96799b2b2d4e510748e330e6d9cc84bac4da
Ahmet ilhan <ailhan@hepsiemlak.com>
2022-11-13 22:58:50 +0300

    readme file updated

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Express IP Blocker
+
+_You can use this package to prevent continuous requests to APIs you write with Express.js within a certain period of time._
--- lib/configs/lib.config.ts
+++ lib/configs/lib.config.ts
@@ -2,5 +2,5 @@ export default {
   limit: 10,
   secretKey: "123y192eh12973y19273y1923y18972y391823yu",
   reqBlockMessage: "Too Many Requests",
-  expire: 1000 * 10,
+  expire: 1000 * 10, // 10 second
 };
