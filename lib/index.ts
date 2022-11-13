87258923916290158ad795eab683775b95e123b1
Ahmet ilhan <ailhan@hepsiemlak.com>
2022-11-13 21:11:00 +0300

    set and get controll added

--- lib/index.ts
+++ lib/index.ts
@@ -1,6 +1,9 @@
+import ip from "ip";
+import { NextFunction, Request, Response } from "express";
+
+import { getCookie, setCookie } from "./utils/cookie.utils";
 import libConfig from "./configs/lib.config";
 import LibConfigType from "./types/lib-config.type";
-import ip from "ip";
 class ExpressIPBlocker {
   private config: LibConfigType = libConfig;
   constructor(public _config: LibConfigType) {
@@ -12,10 +15,20 @@ class ExpressIPBlocker {
       Object.assign(this.config, this._config);
     }
   };
-  checkIP = (): void => {
+  checkIP = (req: Request, res: Response, next: NextFunction): void => {
     const reqIP: any = ip.address();
-    if(!!reqIP){
-        const reqCount = 
+    if (!!reqIP) {
+      let libCookie: any = getCookie(req);
+      if (!libCookie) {
+        const cookieVal = { count: libConfig.count, ip: reqIP };
+        setCookie(req, res, cookieVal);
+        next();
+        return;
+      }
+      libCookie = JSON.parse(libCookie);
+      const remainingCount = Number(libCookie.count) - 1;
+      const newCookieVal = { count: remainingCount, ip: reqIP };
+      setCookie(req, res, newCookieVal);
     }
   };
 }
--- lib/utils/cookie.utils.ts
+++ lib/utils/cookie.utils.ts
@@ -2,17 +2,17 @@ import { CookieOptions, Request, Response } from "express";
 import libConfig from "../configs/lib.config";
 
 export const getCookie = (req: Request): any => {
-  const { cookies } = req;
-  if (cookies) {
-    return cookies[libConfig.secretKey];
-  }
+  return req.cookies[libConfig.secretKey];
 };
 
 export const setCookie = (
   req: Request,
   res: Response,
-  val: string,
-  options: CookieOptions
+  val: object,
+  options?: CookieOptions
 ): void => {
-  res.cookie(libConfig.secretKey, val, options);
+  res.cookie(libConfig.secretKey, JSON.stringify(val), {
+    ...{ maxAge: 100000, httpOnly: true, secure: true },
+    ...options,
+  });
 };
