54fc10115a85a6f7404cc34b170d7e083136b8e4
Ahmet ilhan <ailhan@hepsiemlak.com>
2022-11-13 20:43:55 +0300

    set config func added

--- lib/index.ts
+++ lib/index.ts
@@ -1,3 +1,17 @@
+import libConfig from "./configs/lib.config";
+import LibConfigType from "./types/lib-config.type";
+
 class ExpressIPBlocker {
-  constructor() {}
+  private config: LibConfigType = libConfig;
+  constructor(public _config: LibConfigType) {
+    this.setConfig = this.setConfig.bind(this);
+    this.setConfig();
+  }
+  setConfig = (): void => {
+    if (this._config) {
+      Object.assign(this.config, this._config);
+    }
+  };
 }
+
+export default ExpressIPBlocker;
